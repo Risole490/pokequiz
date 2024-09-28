8f29814b1cafaebd9f751fc6032ae9a8cc1a0de9
Leoyan <71907948+Risole490@users.noreply.github.com>
2024-09-28 10:33:58 -0300

    Resolvendo conflitos

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const port = process.env.PORT || 5000;
 
 const app = express();
 const port = process.env.PORT || 5000;
